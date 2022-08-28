d266f8ede6a5aff8506c64537f7fa16469f1a1f4
Cameron Weaver (Clutch) <lockerzmodding@gmail.com>
2022-08-27 23:11:12 -0400

    Fix multi project

---
+++ API/src/main/java/me/clutchy/gameapi/GameAPI.java
@@ -0,0 +1,18 @@
+package me.clutchy.gameapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
+
+@Plugin(name = "Game API", version = "1.0.0")
+public class GameAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
