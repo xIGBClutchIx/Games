6922909dff15c7598b96ddca5d8f53e1fdb6e6d7
Cameron Weaver (Clutch) <lockerzmodding@gmail.com>
2022-08-29 18:21:28 -0400

    Add finding of a player

--- GameAPI/src/main/kotlin/me/clutchy/games/GameManager.kt
+++ GameAPI/src/main/kotlin/me/clutchy/games/GameManager.kt
@@ -2,10 +2,12 @@ package me.clutchy.games
 
 import org.bukkit.Bukkit
 import org.bukkit.Location
+import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitTask
 import java.util.UUID
 import java.util.function.BiConsumer
+import java.util.function.Consumer
 
 class GameManager(private val api: GameAPI,
                   private val baseTypes: HashMap<String, Class<out Game>> = hashMapOf(), // sg - BASE GAME
@@ -37,46 +39,74 @@ class GameManager(private val api: GameAPI,
     }
 
     // Command - /game kill sg-random
-    fun killGame(player: Player, gameId: String) {
+    fun killGame(sender: Player, gameId: String) {
         if (games.containsKey(gameId) and tasks.containsKey(gameId)) {
             games.remove(gameId)
             tasks.remove(gameId)
+            states.remove(gameId)
+            // Remove from any current games
+            players.forEach { (foundGameID, players) ->
+                players.forEach { player ->
+                    val playerInGame =  Bukkit.getPlayer(player)
+                    playerInGame?.sendMessage("Game killed: $foundGameID")
+                    // Send to Lobby
+                    playerInGame?.sendToLobby()
+                }
+                if (!players.contains(sender.uniqueId)) sender.sendMessage("Game killed: $foundGameID")
+            }
             players.remove(gameId)
         } else {
-            player.sendMessage("Game doesn't exist!")
+            sender.sendMessage("Game doesn't exist!")
         }
     }
 
-    // Command - /game join sg-random name
-    fun addPlayer(game: String, player: Player) {
+    // Command - /game find username
+    fun findPlayer(sender: CommandSender, playerName: String) {
+        // Check for that actual player
+        val player = Bukkit.getPlayer(playerName)
+        if (player != null) {
+            val players = players.filterValues { uuids -> uuids.contains(player.uniqueId) }
+            if (players.isNotEmpty()) {
+                sender.sendMessage("Player found: $players")
+            } else {
+                sender.sendMessage("Player is not in a game!")
+            }
+        }
+    }
+
+    // Command - /game join sg-random username
+    fun addPlayer(sender: CommandSender, gameID: String, player: Player) {
         // Make sure game exists
-        if (games.containsKey(game)) {
+        if (games.containsKey(gameID)) {
             // Remove from any current games
-            players.forEach { (gameID, players) ->
+            players.forEach { (foundGameID, players) ->
                 if (players.remove(player.uniqueId)) {
-                    player.sendMessage("Removed from game: $gameID")
+                    player.sendMessage("Removed from game: $foundGameID")
+                    if (sender != player) sender.sendMessage("Removed (${player.name}) from game: $foundGameID")
                 }
             }
             // Get current players for that game id
-            val listPlayers = players[game]
+            val listPlayers = players[gameID]
             if (listPlayers != null) {
                 // Add to game
-                player.sendMessage("Added to game: $game")
+                player.sendMessage("Added to game: $gameID")
+                if (sender != player) sender.sendMessage("Added (${player.name}) to game: $gameID")
                 listPlayers.add(player.uniqueId)
-                players[game] = listPlayers
-                player.sendToWorld(game)
+                players[gameID] = listPlayers
+                player.sendToWorld(gameID)
             }
         } else {
-            player.sendMessage("Game doesn't exist!")
+            sender.sendMessage("Game doesn't exist!")
         }
     }
 
-    // Command - /game leave name
-    fun removePlayer(player: Player) {
+    // Command - /game leave username
+    fun removePlayer(sender: CommandSender, player: Player) {
         // Remove from any current games
-        players.forEach { (gameID, players) ->
+        players.forEach { (foundGameID, players) ->
             if (players.remove(player.uniqueId)) {
-                player.sendMessage("Removed from game: $gameID")
+                player.sendMessage("Left the game: $foundGameID")
+                if (sender != player) sender.sendMessage("Left (${player.name}) the game: $foundGameID")
             }
         }
         // Send to Lobby
